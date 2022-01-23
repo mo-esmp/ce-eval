573d24781da9aa28f7e0b3d47c06bc866622bce5
Mohsen Esmailpour <mo.esmp@gmail.com>
2022-01-23 18:15:47 +0400

    Add service collection extension.

---
+++ src/CeEval.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using CeEval.Shared.HttpClients;
+using CeEval.Shared.Repositories;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+
+namespace CeEval.Shared.Extensions;
+/// <summary>
+///   The service collection extensions.
+/// </summary>
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    ///   Adds services for the shared project.
+    /// </summary>
+    /// <param name="services">The services.</param>
+    /// <param name="configuration">The configuration.</param>
+    /// <remarks>
+    ///   In "appsettings.json" file the following sections should be available:
+    ///   <para>"ChannelEngineApi": { "BaseUrl": "", "ApiKey": "" }</para>
+    /// </remarks>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    public static IServiceCollection AddCeEval(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IOrderRepository, OrderRepository>();
+        services.AddTransient<ApiKeyDelegatingHandler>();
+
+        services.AddHttpClient("ChannelEngine", httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(configuration.GetSection("ChannelEngineApi:BaseUrl").Value);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }).AddHttpMessageHandler<ApiKeyDelegatingHandler>();
+
+        services.AddMediatR(typeof(ServiceCollectionExtensions));
+
+        return services;
+    }
+}
---
+++ src/CeEval.Shared/HttpClients/ApiKeyDelegatingHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CeEval.Shared.HttpClients;
+
+public class ApiKeyDelegatingHandler : DelegatingHandler
+{
+    private readonly string _apiKey;
+
+    public ApiKeyDelegatingHandler(IConfiguration configuration)
+    {
+        _apiKey = configuration.GetSection("ChannelEngineApi:ApiKey").Value;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        request.RequestUri = new Uri($"{request.RequestUri}&apikey={_apiKey}");
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
