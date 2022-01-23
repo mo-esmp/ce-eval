4b1534729df651d8f9e2fd2b46809cea5668fe7c
Mohsen Esmailpour <mo.esmp@gmail.com>
2022-01-23 17:19:14 +0400

    Implement order repository.

---
+++ src/CeEval.Shared/Repositories/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using CeEval.Shared.Models;
+
+namespace CeEval.Shared.Repositories;
+
+/// <summary>
+///   The order repository.
+/// </summary>
+public interface IOrderRepository
+{
+    /// <summary>
+    ///   Gets the orders with lines by status asynchronous.
+    /// </summary>
+    /// <param name="status">The order status.</param>
+    /// <param name="cancellationToken">The cancellation token (optional.</param>
+    /// <returns>A Task contains list of orders.</returns>
+    public Task<IEnumerable<Order>> GetOrdersWithLinesByStatusAsync(OrderStatus status, CancellationToken cancellationToken = default);
+}
---
+++ src/CeEval.Shared/Repositories/OrderRepository.cs
@@ -0,0 +1,53 @@
+﻿using CeEval.Shared.Models;
+
+namespace CeEval.Shared.Repositories;
+
+/// <inheritdoc/>
+public class OrderRepository : IOrderRepository
+{
+    private readonly HttpClient _httpClient;
+
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="OrderRepository"/> class.
+    /// </summary>
+    /// <param name="httpClientFactor">The HTTP client factory to create <see cref="HttpClient"/>.</param>
+    public OrderRepository(IHttpClientFactory httpClientFactor)
+    {
+        _httpClient = httpClientFactor.CreateClient("ChannelEngine");
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<Order>> GetOrdersWithLinesByStatusAsync(OrderStatus status, CancellationToken cancellationToken = default)
+    {
+        var client = new ChannelEngineRestClient(_httpClient)
+        {
+            BaseUrl = _httpClient.BaseAddress?.ToString()
+        };
+        var orderStatusView = (OrderStatusView)status;
+
+        var response = await client.OrderGetByFilterAsync(statuses: new[] { orderStatusView }, null, null, null, null, null,
+            null, null, null, null, null, null, null, null, cancellationToken);
+
+        if (response?.Content == null)
+            return new List<Order>();
+
+        return response.Content.Select(o => new Order
+        {
+            ChannelId = o.ChannelId,
+            ChannelName = o.ChannelName,
+            Id = o.Id,
+            OrderDate = o.OrderDate,
+            Status = (OrderStatus)o.Status,
+            Lines = o.Lines == null
+                ? new List<OrderLine>()
+                : o.Lines.Select(ol => new OrderLine
+                {
+                    ChannelProductNo = ol.ChannelProductNo,
+                    Description = ol.Description,
+                    Gtin = ol.Gtin,
+                    MerchantProductNo = ol.MerchantProductNo,
+                    Quantity = ol.Quantity
+                })
+        });
+    }
+}
